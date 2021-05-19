6aeaa93fde3004ada6be9bd29977c4e93f8c9885
yoon <seoung59@gmail.com>
2021-05-20 04:09:44 +0900

    Initial Commit

---
+++ assignment/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  assignment
+//
+//  Created by YOON on 2021/05/20.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
